197aa50366b67169e85c2622e3c913acd046cd8e
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-08-30 15:36:14 -0700

    commit tree 90a1909ace3770ccad17c272f48a1b42ba0706a8

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+let num3 = 10;
+let num4 = 6;
+
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*100 + 1);
+
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,13,15,17,20);
+
+
+
+
+/*
+index.js
+    multiply
+      1) is an equation whose multiplied results will equal 62
+    random
+      2) generates a random integer greater than 0
+    mod
+      3) is an equation that calculates a remainder that is equal to 4
+    max
+      4) will return 20 as the highest number in the set
+      
+*/
